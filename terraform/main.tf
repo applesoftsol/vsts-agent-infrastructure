bc4eb9f79ff301959df89bcff49177894234925e
applesoftsol <63569208+applesoftsol@users.noreply.github.com>
2020-04-19 06:54:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "aci-rg" {
 resource "random_id" "randomId" {
   keepers = {
     # Generate a new ID only when a new resource group is defined
-    resource_group = "${azurerm_resource_group.aci-rg.name}"
+    resource_group = "azurerm_resource_group.aci-rg.name"
   }
 
   byte_length = 8
@@ -14,8 +14,8 @@ resource "random_id" "randomId" {
 
 resource "azurerm_storage_account" "aci-sa" {
   name                = "acisa${random_id.randomId.hex}"
-  resource_group_name = "${azurerm_resource_group.aci-rg.name}"
-  location            = "${azurerm_resource_group.aci-rg.location}"
+  resource_group_name = "azurerm_resource_group.aci-rg.name"
+  location            = "azurerm_resource_group.aci-rg.location"
   account_tier        = "Standard"
 
   account_replication_type = "LRS"
@@ -23,16 +23,16 @@ resource "azurerm_storage_account" "aci-sa" {
 
 resource "azurerm_storage_share" "aci-share" {
   name                 = "aci-vsts-share"
-  resource_group_name  = "${azurerm_resource_group.aci-rg.name}"
-  storage_account_name = "${azurerm_storage_account.aci-sa.name}"
+  resource_group_name  = "azurerm_resource_group.aci-rg.name"
+  storage_account_name = "azurerm_storage_account.aci-sa.name"
 
   quota = 50
 }
 
 resource "azurerm_container_group" "aci-vsts" {
   name                = "aci-agent"
-  location            = "${azurerm_resource_group.aci-rg.location}"
-  resource_group_name = "${azurerm_resource_group.aci-rg.name}"
+  location            = "azurerm_resource_group.aci-rg.location"
+  resource_group_name = "azurerm_resource_group.aci-rg.name"
   ip_address_type     = "public"
   os_type             = "linux"
 
@@ -44,20 +44,20 @@ resource "azurerm_container_group" "aci-vsts" {
     port   = "80"
 
     environment_variables {
-      "VSTS_ACCOUNT" = "${var.vsts-account}"
-      "VSTS_TOKEN"   = "${var.vsts-token}"
-      "VSTS_AGENT"   = "${var.vsts-agent}"
-      "VSTS_POOL"    = "${var.vsts-pool}"
+      "VSTS_ACCOUNT" = "var.vsts-account"
+      "VSTS_TOKEN"   = "var.vsts-token"
+      "VSTS_AGENT"   = "var.vsts-agent"
+      "VSTS_POOL"    = "var.vsts-pool"
     }
 
     volume {
       name       = "logs"
       mount_path = "/aci/logs"
       read_only  = false
-      share_name = "${azurerm_storage_share.aci-share.name}"
+      share_name = "azurerm_storage_share.aci-share.name"
 
-      storage_account_name = "${azurerm_storage_account.aci-sa.name}"
-      storage_account_key  = "${azurerm_storage_account.aci-sa.primary_access_key}"
+      storage_account_name = "azurerm_storage_account.aci-sa.name"
+      storage_account_key  = "azurerm_storage_account.aci-sa.primary_access_key"
     }
   }
 
